f0e8fe7aa4749965b0f4c90edfb3e7ba3da3bc85
Horea Crisan <horea.crisan@softvision.ro>
2019-07-24 18:00:36 +0300

    modification @template

--- online-store-flexbox-remake/script.js
+++ online-store-flexbox-remake/script.js
@@ -1,3 +1,15 @@
 (function() {
-    let addToCart = window;
+    let onHover = document.getElementsByClassName("product-page");
+    let addToCart = document.getElementsByClassName("add-to-cart");
+
+    for(let i = 0; i <=onHover.length; i++) {
+        onHover[i].addEventListener("mouseover", function(){
+            addToCart[i].style.opacity = "1";
+        });
+        onHover[i].addEventListener("mouseleave", function(){
+            addToCart[i].style.opacity = "0";
+        });
+    }
+
+
 })();
