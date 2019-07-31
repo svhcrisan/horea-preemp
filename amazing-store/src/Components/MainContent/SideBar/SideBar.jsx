e393567b798393440a14843ae4dfa1dcdd14532e
Horea Crisan <horea.crisan@softvision.ro>
2019-07-31 16:51:43 +0300

    react-template-31iulie

--- amazing-store/src/Components/MainContent/SideBar/SideBar.jsx
+++ amazing-store/src/Components/MainContent/SideBar/SideBar.jsx
@@ -1,21 +1,45 @@
 import React from "react";
 import "./SideBar.css";
 
-const SideBar = props => {
-  const categories = Object.keys(props.categories);
-  console.log(categories);
-  return (
-    <div className="sidebar-container">
-      <div className="all-categories">ALL CATEGORIES</div>
-      {categories.map((category, index) => {
-        return (
-          <button key={index} className="button-category-name">
-            {category}
-          </button>
-        );
-      })}
-    </div>
-  );
+
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: null };
+  }
+
+
+  render() {
+
+    const categories = Object.keys(this.props.categories);
+
+    console.log(categories);
+    const showSubMenu = (categories) => {
+      return categories.map((item, index) => <li key={index}>{item}</li>)
+    }
+
+    return (
+      < div className="sidebar-container" >
+        <div className="all-categories">ALL CATEGORIES</div>
+        {
+          categories.map((category, index) => {
+            return (
+              <div key={index}>
+                <button id={category} className="button-category-name" onClick={() => { this.setState(state => ({ show: this.state.show === category ? null : category })); }}>
+                  {category}
+                </button>
+                <div className="sub-menu">
+                  {
+                    this.state.show === category && showSubMenu(this.props.categories[category])
+                  }
+                </div>
+              </div>
+            );
+          }) //end map
+        }
+      </div >
+    );
+  } // end render
 };
 
 export default SideBar;
